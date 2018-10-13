890203d42bb82f54736e8e3ced72bfe7d6780767
leonardo-mendes <leonardocm92@hotmail.com>
2018-10-13 16:59:45 -0300

    Created all services

--- application/src/main/java/controllers/CarController.java
+++ application/src/main/java/controllers/CarController.java
@@ -2,20 +2,50 @@ package controllers;
 
 import domains.Car;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import services.CarService;
 
 import java.util.List;
 
-@RestController(value = "/car")
+@RestController
+@RequestMapping(value = "/car")
 public class CarController {
 
     @Autowired
     private CarService carService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Car> findAll(){
         return carService.findAll();
     }
+
+    @GetMapping
+    @RequestMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Car findById(@PathVariable Integer id){
+        return carService.findById(id);
+    }
+
+    @PutMapping
+    @RequestMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Car update(@PathVariable Car car){
+        return carService.update(car);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Car insert(@PathVariable Car car){
+        return carService.insert(car);
+    }
+
+    @DeleteMapping
+    @RequestMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        carService.delete(id);
+    }
+
 }
--- service/src/main/java/services/CarService.java
+++ service/src/main/java/services/CarService.java
@@ -2,10 +2,13 @@ package services;
 
 import domains.Car;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import repositories.CarRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarService {
@@ -13,8 +16,42 @@ public class CarService {
     @Autowired
     private CarRepository carRepository;
 
-    public List<Car> findAll(){
+    public List<Car> findAll() {
         return carRepository.findAll();
+    }
+
+    public Car insert(Car car) {
+        return carRepository.save(car);
+    }
+
+    public Car findById(Integer id) {
+        Optional<Car> carUpdate = carRepository.findById(id);
+
+        carUpdate.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND, "Car not found with id " + id));
+
+        return carUpdate.get();
+    }
+
+    public Car update(Car car) {
+        Optional<Car> carUpdate = carRepository.findById(car.getId());
+        Car carUpdated = new Car();
+
+        carUpdate.ifPresent(carOptional -> {
+            carUpdated.setName(carOptional.getName());
+            carUpdated.setId(carOptional.getId());
+        });
+        carUpdate.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND, "Car not found with id " + car.getId()));
+
+
+        return carUpdated;
+    }
+
+    public void delete(Integer id) {
+        Optional<Car> carUpdate = carRepository.findById(id);
+
+        carUpdate.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND, "Car not found with id " + id));
+
+        carRepository.delete(carUpdate.get());
     }
 
 }
